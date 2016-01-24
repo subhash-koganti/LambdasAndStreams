302388b145d49dd9b804b7b23d213a8651293ffe
Subhash Koganti <kogantisubhash@gmail.com>
2016-01-24 00:53:21 -0500

    Test for Performance- Lamdas Suck

--- src/com/ug/codecafe/UGCodeCafeDemo2.java
+++ src/com/ug/codecafe/UGCodeCafeDemo2.java
@@ -1,5 +1,21 @@
 package com.ug.codecafe;
 
+import java.util.List;
+
 public class UGCodeCafeDemo2 {
+
+	public static void main(String[] args) {
+		
+		long startTime = System.currentTimeMillis();
+		List<RateQuote> rateQuoteList1 = RateQuoteDataGenerator.createDummyRateQuotes(2000);
+		long endTime = System.currentTimeMillis();
+
+		long startTimeRegularLoop = System.currentTimeMillis();
+		List<RateQuote> rateQuoteList2 = RateQuoteDataGenerator.createDummyRateQuoteDataUsingRegularLoops(2000);
+		long endTimeRegularLoop = System.currentTimeMillis();
+		
+		System.out.println("Time Taken for Lambdas is " + (endTime-startTime));
+		System.out.println("Time Taken for Regular is " + (endTimeRegularLoop - startTimeRegularLoop));
+	}
 
 }
